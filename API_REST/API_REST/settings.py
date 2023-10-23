d42a1e1a8a0231ab7df9c051f114a2e2f8da111f
Chopperkasa <limaszandresfelipe@gmail.com>
2023-10-23 18:22:09 -0500

    Conexión a base de datos

--- API_REST/API_REST/settings.py
+++ API_REST/API_REST/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'API_REST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apirest',
+        "USER": "Andres",
+        "PASSWORD": "SQLPassword",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
