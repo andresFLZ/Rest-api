99ef97461ada6798f3d4bdef99b7573de1226504
Chopperkasa <limaszandresfelipe@gmail.com>
2023-10-23 23:52:24 -0500

    Implementación de los métodos GET para listar todos los usuarios o uno en especifico

--- API_REST/api/urls.py
+++ API_REST/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TutorialView
+from .views import TutorialView, UsuarioView
 
 urlpatterns = [
     path('tutoriales/', TutorialView.as_view(), name='lista_tutoriales'), # Url para devover todos los turiales
     path('tutoriales/<int:id>', TutorialView.as_view(), name='tutorial'), # Url para devover un tutorial en especifico
+    path('usuarios/', UsuarioView.as_view(), name='lista_usuarios'), # Url para devover todos los usuarios
+    path('usuarios/<int:id>', UsuarioView.as_view(), name='usuario'), # Url para devover un usuario en especifico
 ]
--- API_REST/api/views.py
+++ API_REST/api/views.py
@@ -67,3 +67,30 @@ class TutorialView(View):
         else:
             datos = {'message': "Tutorial no encontrado..."}
         return JsonResponse(datos)
+    
+class UsuarioView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        # Función encargada de permitir los envios POST ignorando el csrf
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        # Implememtación del método GET en la api para recuperar datos de Usuario
+
+        if (id > 0): # Devuele un usuario en especifico dependiendo del id
+            usuarios = list(Usuario.objects.filter(id_usuario=id).values()) # Recupera el usuario de la base de datos
+            if len(usuarios) > 0:
+                usuario = usuarios[0]
+                datos = {'message': "Success", 'company': usuario}
+            else:
+                datos = {'message': "Usuario no encontrado..."}
+            return JsonResponse(datos)
+        else: # Devuelve todos los usuarios 
+            usuarios = list(Usuario.objects.values()) # Recupera los usuarios de la base de datos
+            if len(usuarios) > 0:
+                datos = {'message': "Success", 'companies': usuarios}
+            else:
+                datos = {'message': "usuarios no encontrados..."}
+            return JsonResponse(datos)
